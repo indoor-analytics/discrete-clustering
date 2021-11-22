635908e24429dc5d60f90f281320e51356a137b3
Rémy Raes <contact@remyraes.com>
2021-11-22 13:33:39 +0100

    [feat] cells grid can have a triangle shape

--- src/getCellsFromArea.ts
+++ src/getCellsFromArea.ts
@@ -3,12 +3,15 @@ import {Feature, FeatureCollection, Polygon} from "@turf/helpers";
 import length from "@turf/length";
 import squareGrid from "@turf/square-grid";
 import {Shape} from "./Shape";
+import triangleGrid from "@turf/triangle-grid";
 
 export function getCellsFromArea(zoneOfInterest: Feature<Polygon>, granularity = 42, shape: Shape = Shape.Square): FeatureCollection<Polygon> {
     const areaBbox = bbox(zoneOfInterest);
     const areaLength = length(zoneOfInterest);
     const cellSize = areaLength/granularity;
     switch (shape) {
+    case Shape.Triangle:
+        return triangleGrid(areaBbox, cellSize, {mask: zoneOfInterest});
     default:
         return squareGrid(areaBbox, cellSize, {units: "kilometers", mask: zoneOfInterest});
     }
