cba57f899e2c8b6854fb192c74e5c3cb5aff5038
Rémy Raes <contact@remyraes.com>
2021-11-22 10:18:19 +0100

    [feat] adding basic markCellsWithPaths implementation

--- src/markCellsWithPaths.ts
+++ src/markCellsWithPaths.ts
@@ -1,4 +1,5 @@
-import {Feature, featureCollection, FeatureCollection, LineString, Polygon} from "@turf/helpers";
+import {Feature, FeatureCollection, LineString, Polygon} from "@turf/helpers";
+import lineIntersect from "@turf/line-intersect";
 
 export function markCellsWithPaths(
     cells: FeatureCollection<Polygon>,
@@ -8,5 +9,17 @@ export function markCellsWithPaths(
         throw new RangeError('Cells collection must not be empty.');
     if (paths.length === 0)
         throw new RangeError('Paths array must not be empty.');
-    return featureCollection([]);
+
+    for (const path of paths) {
+        for (const cell of cells.features) {
+            if (lineIntersect(path, cell) !== null) {
+                if (cell.properties?.weight === undefined)
+                    (cell as Feature<Polygon, {weight: number}>).properties.weight = 0;
+                else
+                    cell.properties.weight += 1;
+            }
+        }
+    }
+
+    return cells;
 }
