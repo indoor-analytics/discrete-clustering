0bdd62236a2b3602fcc400fa0c544046ab9d9bf9
Rémy Raes <contact@remyraes.com>
2021-11-22 09:37:16 +0100

    [test] markCellsWithPaths should throw with empty cells collection

--- test/markCellsWithPaths.test.ts
+++ test/markCellsWithPaths.test.ts
@@ -2,6 +2,7 @@ import squareGrid from "@turf/square-grid";
 import {trainStationZoneOfInterest} from "./features/zones";
 import bbox from "@turf/bbox";
 import {markCellsWithPaths} from "../src/markCellsWithPaths";
+import {featureCollection, lineString} from "@turf/helpers";
 
 const cells = squareGrid(bbox(trainStationZoneOfInterest), 10);
 
@@ -9,5 +10,10 @@ describe('Mark cells with paths', () => {
     it ('should throw with empty paths array', () => {
         const markCells = () => markCellsWithPaths(cells, []);
         expect(markCells).toThrowError(new RangeError('Paths array must not be empty.'))
+    });
+
+    it ('should throw with empty cells collection', () => {
+        const markCells = () => markCellsWithPaths(featureCollection([]), [lineString([[0, 1], [0, 2]])]);
+        expect(markCells).toThrowError(new RangeError('Cells collection must not be empty.'))
     });
 });
