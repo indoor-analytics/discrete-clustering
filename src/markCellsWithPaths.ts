ce1dfca3acec26f311034bd1b097a202f685d6ce
Rémy Raes <contact@remyraes.com>
2021-11-22 09:34:59 +0100

    [feat] markCellsWithPaths throws with empty paths array

--- src/markCellsWithPaths.ts
+++ src/markCellsWithPaths.ts
@@ -1,8 +1,11 @@
-import { FeatureCollection, LineString, Polygon } from "@turf/helpers";
+import {Feature, featureCollection, FeatureCollection, LineString, Polygon} from "@turf/helpers";
 
 export function markCellsWithPaths(
     cells: FeatureCollection<Polygon>,
     paths: Feature<LineString>[]
 ): FeatureCollection {
-    // TODO implement
+    console.log(cells);
+    if (paths.length === 0)
+        throw new RangeError('Paths array must not be empty.');
+    return featureCollection([]);
 }
