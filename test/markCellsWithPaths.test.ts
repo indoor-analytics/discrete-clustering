abab5ab5a07b4ca0fb828bccda816e6208320947
Rémy Raes <contact@remyraes.com>
2021-11-22 10:06:50 +0100

    [test] markCellsWithPaths should mark some cells

--- test/markCellsWithPaths.test.ts
+++ test/markCellsWithPaths.test.ts
@@ -3,8 +3,10 @@ import {trainStationZoneOfInterest} from "./features/zones";
 import bbox from "@turf/bbox";
 import {markCellsWithPaths} from "../src/markCellsWithPaths";
 import {featureCollection, lineString} from "@turf/helpers";
+import {getPaths} from "./features/paths";
 
 const cells = squareGrid(bbox(trainStationZoneOfInterest), 0.1);
+const paths = getPaths();
 
 describe('Mark cells with paths', () => {
     it ('should throw with empty paths array', () => {
@@ -15,5 +17,11 @@ describe('Mark cells with paths', () => {
     it ('should throw with empty cells collection', () => {
         const markCells = () => markCellsWithPaths(featureCollection([]), [lineString([[0, 1], [0, 2]])]);
         expect(markCells).toThrowError(new RangeError('Cells collection must not be empty.'))
+    });
+
+    it ('should mark some cells', () => {
+        const allCells = markCellsWithPaths(cells, paths);
+        const markedCells = allCells.features.filter((cell) => cell.properties?.weight !== undefined);
+        expect(markedCells.length).not.toEqual(0);
     });
 });
