3370fbc5b10164a194dad9d84077e8bccb9894d1
Remy Raes <contact@remyraes.com>
2021-11-22 08:17:56 +0100

    [test] using Lille Flandres station as test zone

--- test/getCellsFromArea.test.ts
+++ test/getCellsFromArea.test.ts
@@ -1,10 +1,9 @@
-import { polygon } from "@turf/helpers";
 import { getCellsFromArea } from "../src/getCellsFromArea";
+import { trainStationZoneOfInterest } from "./features/zones";
 
 describe ('Get cells from area', () => {
     it ('should return some cells', () => {
-        const area = polygon([[[0, 1], [1, 1], [1, 0], [0, 0], [0, 1]]]);
-        const cells = getCellsFromArea(area);
+        const cells = getCellsFromArea(trainStationZoneOfInterest);
         expect(cells.features.length).toBeGreaterThan(0);
     });
 });
