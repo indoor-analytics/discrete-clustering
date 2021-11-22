6bbea3de67489e9279c868b52ec1c752d34ad619
Remy Raes <contact@remyraes.com>
2021-11-22 07:53:30 +0100

    [feat] using @turf/length to get area length

--- src/getCellsFromArea.ts
+++ src/getCellsFromArea.ts
@@ -1,10 +1,11 @@
 import bbox from "@turf/bbox";
 import { Feature, FeatureCollection, Polygon } from "@turf/helpers";
+import length from "@turf/length";
 import squareGrid from "@turf/square-grid";
 
 export function getCellsFromArea(zoneOfInterest: Feature<Polygon>): FeatureCollection<Polygon> {
     const areaBbox = bbox(zoneOfInterest);
-    const areaLength = 42; // TODO use @turf/length to get area length
+    const areaLength = length(zoneOfInterest);
     const cellSize = areaLength/12;
     return squareGrid(areaBbox, cellSize, {"units": "kilometers"});
 }
