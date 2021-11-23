4b411603b1a00c7ff0d40117bece51a7efb26fca
Rémy Raes <contact@remyraes.com>
2021-11-23 13:52:53 +0100

    [chore] webpack build produces a commonjs module

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
 const path = require('path');
 
+// eslint-disable-next-line no-undef
 module.exports = {
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        // eslint-disable-next-line no-undef
+        path: path.resolve(__dirname, 'dist'),
+        libraryTarget: "commonjs-module"
+    },
 };
