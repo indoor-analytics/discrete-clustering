6b9d5f89a84acf7035f1fc0ab38133e0a0ddf0cf
Rémy Raes <contact@remyraes.com>
2021-11-23 09:57:54 +0100

    [test] checking discreteClustering computation time

--- test/integration.test.ts
+++ test/integration.test.ts
@@ -5,11 +5,13 @@ import {Shape} from "../src/Shape";
 
 describe('Integration', () => {
     it ('should return some cells', () => {
+        console.time('clustering');
         const cells = discreteClustering(
             getPaths(),
-            180,
+            360,
             Shape.Hexagon
         );
+        console.timeLog('clustering');
         printCollectionToFile(cells);
     });
 });
