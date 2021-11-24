ee7d430396a0c942e313a5ba7624c728ba8309bf
Rémy Raes <contact@remyraes.com>
2021-11-24 15:11:27 +0100

    [test] adding benchmarking script

---
+++ test/benchmarking.ts
@@ -0,0 +1,21 @@
+import {discreteClustering} from "../src";
+import {getPaths} from "./features/paths";
+import {Shape} from "../src/Shape";
+
+const runsCount = 10;
+let durationsSum = 0;
+console.log(`Benchmarking discreteClustering with ${runsCount} executions...\n`);
+
+for (let i=0; i<runsCount; i++) {
+    const beginTime = Date.now();
+    discreteClustering(
+        getPaths(),
+        360,
+        Shape.Hexagon
+    );
+    durationsSum += (Date.now() - beginTime);
+    console.log(`[${new Date().toISOString()}] n°${i} finished`);
+}
+
+const average = durationsSum/runsCount;
+console.log(`\nAverage clustering duration for ${runsCount} runs: `, `${average} ms`);
