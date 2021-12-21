1fb94f09d850e5e8880ab838a5693a927424fa9c
Rémy Raes <contact@remyraes.com>
2021-12-21 13:01:29 +0100

    [refactor] graph conversion to GeoJSON is done by serializing graph

--- src/graph/convertGraphToFeatureCollection.ts
+++ src/graph/convertGraphToFeatureCollection.ts
@@ -1,13 +1,26 @@
-import {featureCollection, FeatureCollection, point} from "@turf/helpers";
+import {Feature, featureCollection, FeatureCollection, point} from "@turf/helpers";
 import Graph from "graph-data-structure";
+interface Serialized {
+    nodes: {
+        id: string;
+    }[];
+    links: {
+        source: string;
+        target: string;
+        weight: string;
+    }[];
+}
 
 export function convertGraphToFeatureCollection (
     graph: typeof Graph
 ): FeatureCollection {
-    return featureCollection(
-        // @ts-ignore
-        graph.nodes().map((node: string) => {
-            return point(node.split(',').map(c => +c))
-        })
-    );
+    // @ts-ignore
+    const serializedGraph: Serialized = graph.serialize();
+
+    const features: Feature[] = [];
+    features.push(...serializedGraph.nodes.map((node) => {
+        return point(node.id.split(',').map(c => +c))
+    }))
+
+    return featureCollection(features);
 }
