c00ab0a8220462cc14cd447f713f3e3b49e213ed
Rémy Raes <contact@remyraes.com>
2021-12-21 10:56:37 +0100

    [test] getClusteredGraph should convert paths to graph containing some nodes

---
+++ test/graph/graphConversion.test.ts
@@ -0,0 +1,14 @@
+import {getClusteredGraph} from "../../src/graph/main";
+import {getPaths} from "../features/paths";
+import {Shape} from "../../src/Shape";
+
+describe('Graph conversion tests', () => {
+    it ('should convert paths to graph containing some nodes', () => {
+        const graph = getClusteredGraph(getPaths(),
+            100,
+            Shape.Hexagon
+        );
+        // @ts-ignore
+        expect(graph.nodes.length).not.toEqual(0);
+    });
+});
