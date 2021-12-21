9a6ac99ff08ad53f16929e923ccc9de4bed47982
Rémy Raes <contact@remyraes.com>
2021-12-21 15:56:11 +0100

    [fix] adjusting Graph methods return types

--- src/graph/types/Graph.d.ts
+++ src/graph/types/Graph.d.ts
@@ -1,14 +1,14 @@
 import {Serialized} from "./Serialized";
 
 export interface Graph {
-    addNode: (node: NodeId) => any;
-    removeNode: (node: NodeId) => any;
+    addNode: (node: NodeId) => Graph;
+    removeNode: (node: NodeId) => Graph;
     nodes: () => NodeId[];
     adjacent: (node: NodeId) => NodeId[];
-    addEdge: (u: NodeId, v: NodeId, weight?: number | undefined) => any;
-    removeEdge: (u: NodeId, v: NodeId) => any;
+    addEdge: (u: NodeId, v: NodeId, weight?: number | undefined) => Graph;
+    removeEdge: (u: NodeId, v: NodeId) => Graph;
     hasEdge: (u: NodeId, v: NodeId) => boolean;
-    setEdgeWeight: (u: NodeId, v: NodeId, weight: EdgeWeight) => any;
+    setEdgeWeight: (u: NodeId, v: NodeId, weight: EdgeWeight) => void;
     getEdgeWeight: (u: NodeId, v: NodeId) => EdgeWeight;
     indegree: (node: NodeId) => number;
     outdegree: (node: NodeId) => number;
@@ -20,5 +20,5 @@ export interface Graph {
         weight?: number | undefined;
     };
     serialize: () => Serialized;
-    deserialize: (serialized: Serialized) => any;
+    deserialize: (serialized: Serialized) => Graph;
 }
