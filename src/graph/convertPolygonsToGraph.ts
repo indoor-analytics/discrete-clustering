aff71a01341f56646005edf2a23a59c82267b85d
Rémy Raes <contact@remyraes.com>
2021-12-21 11:07:11 +0100

    [fix] convertPolygonsToGraph computes a centroid position for each cell polygon

--- src/graph/convertPolygonsToGraph.ts
+++ src/graph/convertPolygonsToGraph.ts
@@ -1,5 +1,6 @@
 import Graph from "graph-data-structure";
 import {Feature, FeatureCollection, Polygon} from "@turf/helpers";
+import centroid from "@turf/centroid";
 
 export function convertPolygonsToGraph(
     cells: FeatureCollection
@@ -8,8 +9,7 @@ export function convertPolygonsToGraph(
 
     // adding a node for each polygon
     for (const polygon of cells.features) {
-        const cell = polygon as Feature<Polygon>;
-        const coordinates = `${cell.geometry.coordinates}`;
+        const coordinates = `${centroid(polygon as Feature<Polygon>).geometry.coordinates}`;
         graph.addNode(coordinates);
     }
 
