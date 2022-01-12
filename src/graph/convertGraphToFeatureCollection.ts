987563a2c749035c94130a413e4b39ac6dddc2d8
Rémy Raes <contact@remyraes.com>
2022-01-12 10:49:12 +0100

    [feat] graph lines' width depends on their weight

--- src/graph/convertGraphToFeatureCollection.ts
+++ src/graph/convertGraphToFeatureCollection.ts
@@ -14,14 +14,28 @@ export function convertGraphToFeatureCollection (
         features.push(...serializedGraph.nodes.map((node) => {
             return point(node.id.split(',').map(c => +c))
         }));
-    features.push(...serializedGraph.links.map(link => {
+
+    // looking for maximum weight
+    let localMaximum = 0;
+    for (const link of serializedGraph.links)
+        if (link.weight > localMaximum)
+            localMaximum = link.weight;
+
+    const MAXIMUM_WIDTH = 10;
+    const lines = serializedGraph.links.map(link => {
         return lineString(
             [
                 link.source.split(',').map(c => +c),
                 link.target.split(',').map(c => +c)
-            ]
+            ],
+            {
+                'stroke-width': (link.weight / localMaximum) * MAXIMUM_WIDTH
+            }
         );
-    }));
+    });
+
+
+    features.push(...lines);
 
     return featureCollection(features);
 }
