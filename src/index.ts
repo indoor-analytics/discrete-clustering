68a390e3f7b1c9ae9fa1e40d8ec51efe830e2d13
Rémy Raes <contact@remyraes.com>
2022-01-14 11:14:50 +0100

    [build] methods are directly exposed (without name) in package

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,21 @@ import { markCellsWithPaths } from "./markCellsWithPaths";
 import {colorCells} from "./colorCells";
 import {Shape} from "./Shape";
 import envelope from "@turf/envelope";
+import { getClusteredGraph } from "./graph/main";
 
 /**
  * Entrypoint of this package.
- * This will compute the smallest polygon containing all input paths, discretize 
- * it using provided shape and shape size, and will compute intersections of 
- * paths with grid cells; each intersected cell will be marked with a weight 
+ * This will compute the smallest polygon containing all input paths, discretize
+ * it using provided shape and shape size, and will compute intersections of
+ * paths with grid cells; each intersected cell will be marked with a weight
  * indicating the number of paths that cross it, and an according color opacity.
- * 
+ *
  * @param paths input paths to cluster
  * @param granularity size of grid cells (increase this to reduce cells' size)
  * @param shape shape used to create grid cells
  * @returns weightened cells
  */
-export function discreteClustering(
+function discreteClustering(
     paths: Feature<LineString>[],
     granularity: number,
     shape = Shape.Square
@@ -28,3 +29,4 @@ export function discreteClustering(
     return colorCells(markedCells as FeatureCollection<Polygon, {weight: number}>);
 }
 
+export {discreteClustering, getClusteredGraph};
