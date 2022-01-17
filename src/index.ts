167ed3fe78aae3a337b7ccf10116744371a88e72
Rémy Raes <contact@remyraes.com>
2022-01-17 12:55:02 +0100

    [build] include getClusteredPath in exported methods

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { getClusteredGraph } from "./graph/getClusteredGraph";
+import { getClusteredPath } from "./graph/getClusteredPath";
 import { Shape } from "./utils/Shape";
 import { clusterPaths } from "./zones/clusterPaths";
 
 /**
  * All methods exposed by the compiled library are exported here.
  */
-export {clusterPaths, getClusteredGraph, Shape};
+export {clusterPaths, getClusteredGraph, getClusteredPath, Shape};
