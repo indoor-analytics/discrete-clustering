ac33238ac6d6c313768e1bf81221ce789089b352
Remy Raes <raes.remy@gmail.com>
2022-01-19 13:54:42 +0100

    [test] getClusteredPath should return a path for all shapes

--- test/graph/getClusteredPath.test.ts
+++ test/graph/getClusteredPath.test.ts
@@ -18,4 +18,21 @@ describe('getClusteredPath', () => {
         );
         expect(path.geometry.coordinates.length).toBeGreaterThan(0);
     });
+
+    it ('should return a path for all shapes', () => {
+        for (const shape of [Shape.Square, Shape.Hexagon, Shape.Triangle]) {
+            const testGraph = getClusteredGraph(getPaths(),
+                60,
+                shape
+            );
+
+            const path = getClusteredPath(
+                testGraph,
+                referencePath.geometry.coordinates[0],
+                referencePath.geometry.coordinates[referencePath.geometry.coordinates.length-1]
+            );
+
+            expect(path.geometry.coordinates.length).toBeGreaterThan(0);
+        }
+    });
 });
