a321272d1bd8496fdcc8cf8512d81a849f1c126d
Rémy Raes <contact@remyraes.com>
2022-02-02 14:50:49 +0100

    [test] removing throwing check test
    Since all cells are marked properly, there are no separate sub-graphs anymore,
    implying that extracting a path is always possible, thus no errors should be
    thrown anymore.

--- test/graph/getClusteredPath.test.ts
+++ test/graph/getClusteredPath.test.ts
@@ -1,24 +1,9 @@
 import {getClusteredGraph, Shape} from "../../src";
 import {getPaths, getReferencePath} from "../features/paths";
-import {getClusteredPath} from "../../src/graph/getClusteredPath";
+import {getClusteredPath} from "../../src";
 
 describe('getClusteredPath', () => {
     const referencePath = getReferencePath();
-
-    it ("should throw when path cannot be extracted from graph", () => {
-        const testGraph = getClusteredGraph(getPaths('oldRuns.json'),
-            360,
-            Shape.Hexagon
-        );
-
-        const doIt = () => getClusteredPath(
-            testGraph,
-            referencePath.geometry.coordinates[0],
-            referencePath.geometry.coordinates[referencePath.geometry.coordinates.length-1]
-        );
-
-        expect(doIt).toThrow(new RangeError('Path cannot be extracted from graph, try using a smaller granularity.'));
-    });
 
     it ('should return a path for all shapes', () => {
         for (const shape of [Shape.Square, Shape.Hexagon, Shape.Triangle]) {
