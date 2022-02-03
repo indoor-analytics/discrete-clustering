cf8991991205ab909d38eb944002f55dc4a4d931
Remy Raes <contact@remyraes.com>
2022-02-03 23:09:07 +0100

    [feat] add splitPolygon implementation

--- src/recursive/splitPolygon.ts
+++ src/recursive/splitPolygon.ts
@@ -1,7 +1,27 @@
-import { Feature, FeatureCollection, Polygon } from "@turf/helpers";
+import centroid from "@turf/centroid";
+import { Feature, featureCollection, FeatureCollection, polygon, Polygon, Position } from "@turf/helpers";
 
 export function splitPolygon(
-    _polygon: Feature<Polygon>
+    iPolygon: Feature<Polygon>
 ): FeatureCollection<Polygon> {
-    throw new Error('Not implemented');
+    if (iPolygon.geometry.coordinates[0].length !== 5)
+        throw new Error('Cannot split this shape.');
+
+    const subZones: Feature<Polygon>[] = [];
+    const boxCenter = centroid(iPolygon);
+
+    for (let i=0; i<4; i++) {
+        const boxVertex = iPolygon.geometry.coordinates[0][i];
+        const newVertex1: Position = [boxCenter.geometry.coordinates[0], boxVertex[1]];
+        const newVertex2: Position = [boxVertex[0], boxCenter.geometry.coordinates[1]];
+        subZones.push(
+            polygon(
+                [ 
+                    [ boxVertex, newVertex1, boxCenter.geometry.coordinates, newVertex2, boxVertex ]
+                ]
+            )
+        );
+    }
+
+    return featureCollection(subZones);
 }
