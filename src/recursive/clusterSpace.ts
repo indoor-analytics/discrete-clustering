a1aade4c8f493b5b774f951e12ca88efeee7167a
Remy Raes <contact@remyraes.com>
2022-02-03 19:20:16 +0100

    [feat] add clusterSpace method skeleton

--- src/recursive/clusterSpace.ts
+++ src/recursive/clusterSpace.ts
@@ -1,13 +1,37 @@
-import { Feature, FeatureCollection, LineString, Polygon } from "@turf/helpers";
+import { Feature, featureCollection, FeatureCollection, LineString, Polygon } from "@turf/helpers";
+import { splitPolygon } from "./splitPolygon";
 
 export function clusterSpace (
-    _paths: Feature<LineString>,
-    _zone: Feature<Polygon>,
-    _targetDepth: number,
-    _currentDepth = 0
+    _paths: FeatureCollection<LineString>,
+    zone: Feature<Polygon>,
+    targetDepth: number,
+    currentDepth = 0
 ): FeatureCollection<Polygon, {weight: number}> {
     // TODO ensure that targetDepth >= 1
     // TODO throw if there are paths outside zone
+    // TODO if there are no paths, return empty collection
 
-    throw new Error('Not implemented');
+    // TODO mark zone with weight
+
+    // end recursion if target depth has been reached
+    if (currentDepth === targetDepth)
+        return featureCollection([zone]);
+
+    
+    const subZones = splitPolygon(zone).features;
+    const returnCells: Feature<Polygon, {weight: number}>[] = [];
+
+    for (const subZone of subZones) {
+        const subZonePaths: FeatureCollection<LineString> = featureCollection([]);   // TODO get subZone path segments
+        returnCells.push(
+            ...clusterSpace(
+                subZonePaths,
+                subZone,
+                targetDepth,
+                currentDepth + 1
+            ).features
+        );
+    }
+
+    return featureCollection(returnCells);
 }
