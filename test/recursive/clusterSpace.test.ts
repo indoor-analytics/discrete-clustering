e3772feb088ae7c59b07a4ddd9ca5a988475aeb8
Remy Raes <raes.remy@gmail.com>
2022-02-04 10:32:27 +0100

    [test] clusterSpace should throw with target depth = 0

---
+++ test/recursive/clusterSpace.test.ts
@@ -0,0 +1,13 @@
+import {getPaths} from "../features/paths";
+import {featureCollection} from "@turf/helpers";
+import {clusterSpace} from "../../src/recursive/clusterSpace";
+import {trainStationSquare} from "../features/zones";
+
+describe('Cluster space', () => {
+    const paths = featureCollection(getPaths());
+
+    it ('should throw with target depth = 0', () => {
+        const cluster = () => clusterSpace(paths, trainStationSquare, 0);
+        expect(cluster).toThrow(new RangeError('Target depth must be superior to 0.'));
+    });
+});
