3a06fa134a76d5e56b557267fcf615c80bdc33c2
Remy Raes <raes.remy@gmail.com>
2022-02-07 15:31:44 +0100

    [test] getPathsEnvelope should return a square envelope (2)

--- test/recursive/getPathsEnvelope.test.ts
+++ test/recursive/getPathsEnvelope.test.ts
@@ -23,4 +23,13 @@ describe('getPathsEnvelope', () => {
         const line2 = lineString([envelope.geometry.coordinates[0][1], envelope.geometry.coordinates[0][2]]);
         expect(lineDistance(line1)).toBeCloseTo(lineDistance(line2));
     });
+
+    it ('should return a square envelope (2)', () => {
+        const envelope = getPathsEnvelope(featureCollection(getPaths('topRuns.json')), Shape.Square);
+        expect(area(envelope)).not.toEqual(0);
+        expect(envelope.geometry.coordinates[0].length).toEqual(5);
+        const line1 = lineString([envelope.geometry.coordinates[0][0], envelope.geometry.coordinates[0][1]]);
+        const line2 = lineString([envelope.geometry.coordinates[0][1], envelope.geometry.coordinates[0][2]]);
+        expect(lineDistance(line1)).toBeCloseTo(lineDistance(line2));
+    });
 });
