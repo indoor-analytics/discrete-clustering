9d4825c90a6cebfd835476cc35ac635065978539
Remy Raes <raes.remy@gmail.com>
2022-02-07 17:35:45 +0100

    [test] use convertPolygonsToGraph in tests instead of getClusteredGraph method

--- test/graph/graphConversion.test.ts
+++ test/graph/graphConversion.test.ts
@@ -1,15 +1,14 @@
-import {getClusteredGraph} from "../../src/graph/getClusteredGraph";
 import {getPaths} from "../features/paths";
 import {Shape} from "../../src/utils/Shape";
 import {printCollectionToFile} from "../utils/printCollectionToFile";
 import {convertGraphToFeatureCollection} from "../../src/graph/convertGraphToFeatureCollection";
+import {clusterPaths, convertPolygonsToGraph} from "../../src";
+import {FeatureCollection, Polygon} from "@turf/helpers";
 
 describe('Graph conversion tests', () => {
     it ('should convert paths to graph containing some nodes', () => {
-        const graph = getClusteredGraph(getPaths(),
-            100,
-            Shape.Hexagon
-        );
+        const cells = clusterPaths(getPaths(), 100, Shape.Hexagon);
+        const graph = convertPolygonsToGraph(cells as FeatureCollection<Polygon, {weight: number}>);
         // @ts-ignore
         expect(graph.nodes().length).not.toEqual(0);
     });
@@ -17,10 +16,8 @@ describe('Graph conversion tests', () => {
 
 describe ('Graph to FeatureCollection', () => {
     it ('should convert graph to featureCollection', () => {
-        const graph = getClusteredGraph(getPaths(),
-            360,
-            Shape.Hexagon
-        );
+        const cells = clusterPaths(getPaths(), 360, Shape.Hexagon);
+        const graph = convertPolygonsToGraph(cells as FeatureCollection<Polygon, {weight: number}>);
         const collection = convertGraphToFeatureCollection(graph);
         printCollectionToFile(collection, 'graph.json');
     });
