4e1669269616e3e1edf1fe8acc371cdb6e2398e0
Remy Raes <raes.remy@gmail.com>
2022-02-07 15:25:20 +0100

    [feat] getPathsEnvelope can create square envelopes

--- src/recursive/getPathsEnvelope.ts
+++ src/recursive/getPathsEnvelope.ts
@@ -1,10 +1,30 @@
 import {Feature, FeatureCollection, LineString, Polygon} from "@turf/helpers";
 import {Shape} from "../utils/Shape";
 import envelope from "@turf/envelope";
+import centroid from "@turf/centroid";
+import distance from "@turf/distance";
+import bbox from "@turf/bbox";
+import circle from "@turf/circle";
+import bboxPolygon from "@turf/bbox-polygon";
 
 export function getPathsEnvelope(
     paths: FeatureCollection<LineString>,
-    _shape: Shape
+    shape: Shape
 ): Feature<Polygon> {
-    return envelope(paths);
+    const pathsEnvelope = envelope(paths);
+
+    if (shape === Shape.Fit) {
+        return pathsEnvelope;
+    } else if (shape === Shape.Square) {
+        const center = centroid(pathsEnvelope);
+        let maxDistance = 0;
+        paths.features.forEach(path => path.geometry.coordinates.forEach(pos => {
+            const dist = distance(pos, center);
+            if (dist > maxDistance)
+                maxDistance = dist;
+        }));
+        return bboxPolygon(bbox(circle(center, maxDistance)));
+    } else {
+        throw new Error('unimplemented');
+    }
 }
