cb7b879dad60f3938fea7974046cbce443e3716e
Remy Raes <contact@remyraes.com>
2022-02-08 16:16:28 +0100

    [refactor] splitPolygon throws if input polygon matched no condition

--- src/recursive/splitPolygon.ts
+++ src/recursive/splitPolygon.ts
@@ -4,24 +4,27 @@ import { Feature, featureCollection, FeatureCollection, polygon, Polygon, Positi
 export function splitPolygon(
     iPolygon: Feature<Polygon>
 ): FeatureCollection<Polygon> {
-    if (iPolygon.geometry.coordinates[0].length !== 5)
-        throw new Error('Cannot split this shape.');
 
-    const subZones: Feature<Polygon>[] = [];
-    const boxCenter = centroid(iPolygon);
-
-    for (let i=0; i<4; i++) {
-        const boxVertex = iPolygon.geometry.coordinates[0][i];
-        const newVertex1: Position = [boxCenter.geometry.coordinates[0], boxVertex[1]];
-        const newVertex2: Position = [boxVertex[0], boxCenter.geometry.coordinates[1]];
-        subZones.push(
-            polygon(
-                [ 
-                    [ boxVertex, newVertex1, boxCenter.geometry.coordinates, newVertex2, boxVertex ]
-                ]
-            )
-        );
+    // square
+    if (iPolygon.geometry.coordinates[0].length === 5) {
+        const subZones: Feature<Polygon>[] = [];
+        const boxCenter = centroid(iPolygon);
+    
+        for (let i=0; i<4; i++) {
+            const boxVertex = iPolygon.geometry.coordinates[0][i];
+            const newVertex1: Position = [boxCenter.geometry.coordinates[0], boxVertex[1]];
+            const newVertex2: Position = [boxVertex[0], boxCenter.geometry.coordinates[1]];
+            subZones.push(
+                polygon(
+                    [ 
+                        [ boxVertex, newVertex1, boxCenter.geometry.coordinates, newVertex2, boxVertex ]
+                    ]
+                )
+            );
+        }
+    
+        return featureCollection(subZones);
     }
 
-    return featureCollection(subZones);
+    throw new Error('Cannot split this shape.');
 }
