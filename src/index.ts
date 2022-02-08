022e5db7ea61ac12cbe2e20e142abd9dc45e67b4
Remy Raes <contact@remyraes.com>
2022-02-08 08:12:10 +0100

    [feat] add clusterSpace method to package exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { convertPolygonsToGraph } from "./graph/convertPolygonsToGraph";
 import { getClusteredPath } from "./graph/getClusteredPath";
+import { clusterSpace } from "./recursive/clusterSpace";
 import { Shape } from "./utils/Shape";
 import { clusterPaths } from "./zones/clusterPaths";
 
 /**
  * All methods exposed by the compiled library are exported here.
  */
-export {clusterPaths, convertPolygonsToGraph, getClusteredPath, Shape};
+export {clusterPaths, clusterSpace, convertPolygonsToGraph, getClusteredPath, Shape};
