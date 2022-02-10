e23002c8bca6c44edd862a9c8ca796b7d0b03d41
Rémy Raes <contact@remyraes.com>
2022-02-10 11:09:52 +0100

    [test] getClusteredPath should return a path with square shape

---
+++ test/utils/getClusteredPath.test.ts
@@ -0,0 +1,16 @@
+import {featureCollection, Position} from "@turf/helpers";
+import {getPaths, getReferencePath} from "../features/paths";
+import {getClusteredPath} from "../../src/utils/getClusteredPath";
+import {Shape} from "../../src";
+
+describe('getClusteredPath', () => {
+    const paths = featureCollection(getPaths());
+    const referencePath = getReferencePath();
+    const startPos: Position = referencePath.geometry.coordinates[0];
+    const endPos: Position = referencePath.geometry.coordinates[referencePath.geometry.coordinates.length-1];
+
+    it ('should return a path with square shape', () => {
+        const path = getClusteredPath(paths, startPos, endPos, 5, Shape.Square);
+        expect(path.geometry.coordinates.length).not.toEqual(0);
+    });
+});
