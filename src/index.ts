cb0e0a5d869aa66d2120e2b8b5ef402c7cd9ae17
Rémy Raes <contact@remyraes.com>
2022-02-10 11:52:50 +0100

    [feat] add getClusteredPath method to package exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { convertPolygonsToGraph } from "./graph/convertPolygonsToGraph";
 import { getClusteredPathFromGraph } from "./graph/getClusteredPathFromGraph";
 import { clusterSpace } from "./recursive/clusterSpace";
+import { getClusteredPath } from "./utils/getClusteredPath";
 import { Shape } from "./utils/Shape";
 import { clusterPaths } from "./zones/clusterPaths";
 
 /**
  * All methods exposed by the compiled library are exported here.
  */
-export {clusterPaths, clusterSpace, convertPolygonsToGraph, getClusteredPathFromGraph, Shape};
+export {clusterPaths, clusterSpace, convertPolygonsToGraph, getClusteredPath, getClusteredPathFromGraph, Shape};
