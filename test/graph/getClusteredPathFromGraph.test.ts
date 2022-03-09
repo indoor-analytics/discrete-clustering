598378e08cc077a352a633c2007ab9faabadc703
Rémy Raes <contact@remyraes.com>
2022-03-09 15:13:03 +0100

    [test] getClusteredPathFromGraph should throw with a non-existing starting node

--- test/graph/getClusteredPathFromGraph.test.ts
+++ test/graph/getClusteredPathFromGraph.test.ts
@@ -5,9 +5,10 @@ import {getEdgeNodesFromPaths} from "../../src/graph/getEdgeNodesFromPaths";
 import {featureCollection} from "@turf/helpers";
 
 describe('getClusteredPathFromGraph', () => {
+    const paths = getPaths();
+
     it ('should return a path for all shapes', () => {
         for (const shape of [Shape.Square, Shape.Hexagon, Shape.Triangle]) {
-            const paths = getPaths();
             const cells = clusterPaths(paths, 60, shape);
             const testGraph = convertPolygonsToGraph(cells);
             const positions = getEdgeNodesFromPaths(testGraph, featureCollection(paths));
@@ -20,5 +21,13 @@ describe('getClusteredPathFromGraph', () => {
 
             expect(path.geometry.coordinates.length).toBeGreaterThan(0);
         }
+    });
+
+    it ('should throw with a non-existing starting node', () => {
+        const cells = clusterPaths(paths, 60, Shape.Triangle);
+        const testGraph = convertPolygonsToGraph(cells);
+        const positions = getEdgeNodesFromPaths(testGraph, featureCollection(paths));
+        const getPath = () => getClusteredPathFromGraph(testGraph, 'hellothere', positions.end);
+        expect(getPath).toThrow(new RangeError('"hellothere" is not a graph edge.'));
     });
 });
